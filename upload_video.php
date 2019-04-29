7307f1ee59a7311963516734ba15b62a98ac13cc
Hiren B Donda <dondahiren2@gmail.com>
2019-04-29 12:40:31 +0530

    adding new file

---
+++ upload_video.php
@@ -0,0 +1,97 @@
+<?php
+	include 'config.php';
+	//require_once 'S3.php';
+	include 's3/vendor/autoload.php';
+	use Aws\S3\S3Client;
+	use Aws\S3\Exception\S3Exception;
+	// AWS Info
+	$bucketName = bucket;
+	$IAM_KEY = awsAccessKey;
+	$IAM_SECRET = awsSecretKey;
+
+	date_default_timezone_set('Asia/Kolkata');
+	$sdate = date("YmdHis");
+	$types = $_POST['type'];
+	$tal_id = $_POST['tal_ids'];
+	$parts = $_POST['parts'];
+	$NameOfFile = $_POST['NameOfFile'];
+	$file_name = $_POST['file_name'];
+	$unique_id = $_POST['unique_id'];
+	$folder_type = $_POST['folder_type'];
+	$department = $_POST['department'];
+	
+	if($folder_type == 'personal' && $department == 'Team'){
+		$target_file = 'uploads/'.$types.'/'.$tal_id.'/'.$department.'/'.$unique_id.'/'.$parts.'/';
+	} else {
+		$target_file = 'uploads/'.$types.'/'.$tal_id.'/'.$parts.'/';
+	}
+	
+ 
+	if(file_exists($target_file)){
+	} else {
+		mkdir($target_file,0755, true);
+	}
+  	
+ 
+	$image_name  = $_FILES[$file_name]['name'];
+	$size 		 = $_FILES[$file_name]['size'];
+	$type 		 = $_FILES[$file_name]['type'];
+	$tmp_name	 = $_FILES[$file_name]['tmp_name'];
+
+	$allowed =  array('jpg','jpeg','pdf','mp4');
+	$ext = strtolower(pathinfo($image_name, PATHINFO_EXTENSION));
+	$file = basename($NameOfFile.'_'.$sdate.'.'.$ext);
+	$target_filename = $target_file.$file;
+  
+	if(in_array($ext,$allowed)){ 
+		try { 
+			$s3 = S3Client::factory(
+				array(
+					'credentials' => array(
+						'key' => awsAccessKey,
+						'secret' => awsSecretKey
+					),
+					'version' => 'latest',
+					'region'  => region
+				)
+			);
+		} catch (Exception $e) { 
+			echo 2;
+		}
+
+		// Add it to S3
+		try { 
+			if($s3->putObject(
+				array(
+					'Bucket'=> bucket,
+					'Key' =>  $target_filename,
+					'SourceFile' => $tmp_name, 
+		    	    'ACL'    => 'public-read'
+				)
+			)){
+				echo $target_filename;
+			}
+			
+		} catch (S3Exception $e) { 
+			echo 3;
+		} catch (Exception $e) { 
+			echo 3;
+		}
+
+		/*if(move_uploaded_file($tmp_name, $target_filename)){
+			$s3 = new S3(awsAccessKey, awsSecretKey);
+			if($s3->putObjectFile($target_filename, bucket, $target_filename, S3::ACL_PUBLIC_READ)){
+				unlink($target_filename);
+				echo $target_filename.'splits'.$ext;
+			}else{
+				echo 2;
+			}
+		} else {
+			echo 2;
+		}*/
+
+	} else {
+		echo 4;
+	}
+	
+?>
---
+++ upload_visiting.php
@@ -0,0 +1,57 @@
+<?php
+	include 'config.php';
+	require_once 'S3.php';
+	date_default_timezone_set('Asia/Kolkata');
+	$sdate = date("YmdHis");
+	$types = $_POST['type'];
+	$tal_id = $_POST['tal_ids'];
+	$parts = $_POST['parts'];
+	$NameOfFile = $_POST['NameOfFile'];
+	$file_name = $_POST['file_name'];
+	
+	
+	$target_file = 'uploads/'.$types.'/'.$tal_id.'/'.$parts.'/';	
+	if(file_exists($target_file)){
+	} else {
+		mkdir($target_file,0755, true);
+	}
+  	
+	
+	$image_name  = $_FILES[$file_name]['name'];
+	$tmp_name	 = $_FILES[$file_name]['tmp_name'];
+ 
+	
+	$ans = '';
+	$ext = '';
+	$image_path = ''; 
+	$c = count($image_name);
+	$allowed =  array('jpg','jpeg','pdf'); 
+	for($i=0;$i<$c;$i++){
+		$i_name		 = $_FILES[$file_name]['name'][$i];
+		$size 		 = $_FILES[$file_name]['size'][$i];
+		$type 		 = $_FILES[$file_name]['type'][$i];
+		$tmp_name	 = $_FILES[$file_name]['tmp_name'][$i];
+		 
+		$ext = strtolower(pathinfo($i_name, PATHINFO_EXTENSION));
+		
+		$file = basename($NameOfFile.'_'.$sdate.$i.'.'.$ext);
+		$target_filename = $target_file.$file;
+		if(in_array($ext,$allowed)){
+			if(move_uploaded_file($tmp_name, $target_filename)){
+				$s3 = new S3(awsAccessKey, awsSecretKey);
+				if($s3->putObjectFile($target_filename, bucket, $target_filename, S3::ACL_PUBLIC_READ)){
+					unlink($target_filename);
+					$image_path .= $target_filename.'splits';
+					$ans = 1;
+				}else{
+					$ans = 2;
+				}
+			} else {
+				$ans = 2;
+			}
+		} else {
+			$ans = 3;
+		}
+	} 
+	echo $ans.'split_visiting'.substr($image_path,0,-6);
+?>
